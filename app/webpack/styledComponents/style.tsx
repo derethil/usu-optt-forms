219172443b2e6bcd7b842981b55107c3b5e858f2
Jaren Glenn <jarenglenn@gmail.com>
2021-08-02 13:07:42 -0600

    styled text input

--- app/webpack/styledComponents/style.tsx
+++ app/webpack/styledComponents/style.tsx
@@ -60,18 +60,60 @@ export const Title = styled.h1`
 // Inputs
 
 export const Label = styled.label`
-  margin-right: auto;
-  /* font-family: Roboto, sans-serif; */
-  /* padding-right: 1em; */
+  position: absolute;
+  top: 0;
+  display: block;
+  transition: 0.2s;
+  font-size: 1rem;
+  color: ${Color.neutrals.grayDark};
 `;
 
 export const InputContainer = styled.div`
-  display: flex;
-  justify-content: flex-end;
+  position: relative;
+  padding: 15px 0 0;
+  margin-bottom: 20px;
+  width: 100%;
 `;
 
 export const Input = styled.input`
-  width: 250px;
+  font-family: inherit;
+  width: 100%;
+  border: 0;
+  border-bottom: 2px solid ${Color.neutrals.gray};
+  outline: 0;
+  font-size: 1.3rem;
+  color: ${Color.neutrals.grayDarker};
+  padding: 7px 0;
+  background: transparent;
+  transition: border-color 0.2s;
+
+  &::placeholder {
+    color: transparent;
+  }
+
+  &:placeholder-shown ~ label {
+    cursor: text;
+    top: 20px;
+  }
+
+  &:focus {
+    ~ & {
+      position: absolute;
+      top: 0;
+      display: block;
+      transition: 0.2s;
+      font-size: 1rem;
+      color: $primary;
+      font-weight:700;
+    }
+    padding-bottom: 6px;
+    font-weight: 700;
+    border-width: 3px;
+    border-image: linear-gradient(to right, ${Color.blues.blueDark},${Color.blues.blue});
+    border-image-slice: 1;
+  }
+
+  &:required,&:invalid { box-shadow:none; }
 
 `;
 
