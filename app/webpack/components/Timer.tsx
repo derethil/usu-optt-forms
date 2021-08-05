1d27d25ebf006cd93d9b4b84dd480ca7711b7b4c
Jaren Glenn <jarenglenn@gmail.com>
2021-08-05 16:03:51 -0600

    styled timer

--- app/webpack/components/Timer.tsx
+++ app/webpack/components/Timer.tsx
@@ -1,28 +1,90 @@
-import React from "react";
+import React, { useRef } from "react";
+import styled, { CSSProperties } from "styled-components";
 
 import { formatTime } from "../utils";
 import { ITimer } from "../types";
+
+import { Button } from "../styledComponents/style";
+import { Color } from "../styledComponents/colors";
+
+const TimerDisplay = styled.h2`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  font-size: 2rem;
+  flex-grow: 1;
+
+  border-radius: 0.75em;
+  border: 3px solid ${Color.neutrals.grayDark};
+
+  background-color: ${Color.blues.blueLight};
+
+  margin: 0;
+
+  margin-right: 1em;
+`;
+
+const TimerContent = styled.div`
+  display: flex;
+  justify-content: stretch;
+  align-items: stretch;
+`;
+
+const TimerButton = styled(Button)`
+  flex-grow: 1;
+  font-weight: 600;
+  font-size: 1.5rem;
+  color: ${Color.lights.white};
+
+  :nth-child(2) {
+    margin-right: 1em;
+  }
+
+`;
 
 
 const Timer = ({ timer }: { timer: ITimer }) => {
 
   const currentButton = () => {
+
+    const getButton = (text: string, onClick: () => void) => {
+      return (
+        <TimerButton
+          color={Color.blues.blue}
+          onClick={onClick}
+          style={{ minWidth: "12em", border: `3px solid ${Color.neutrals.grayDark}` }}
+        >
+          {text}
+        </TimerButton>
+      )
+    }
+
     if (!timer.isActive) {
-      return <button onClick={timer.handleStart}>Start</button>
+      return getButton("Start", timer.handleStart);
+
     } else if (timer.isPaused) {
-      return <button onClick={timer.handleResume}>Resume</button>
+      return getButton("Resume", timer.handleResume);
+
     } else {
-      return <button onClick={timer.handlePause}>Pause</button>
+      return getButton("Pause", timer.handlePause);
     }
   }
 
-  return <div className='stopwatch-card'>
-    <p>{formatTime(timer.timer)}</p>
-    <div className='buttons'>
-      {currentButton()}
-      <button onClick={timer.handleReset} disabled={!timer.isActive}>Reset</button>
-    </div>
-  </div>
+  return <TimerContent>
+    <TimerDisplay>{formatTime(timer.timer)}</TimerDisplay>
+
+    {currentButton()}
+
+    <TimerButton
+      onClick={timer.handleReset}
+      color={Color.accents.brick}
+      style={{ border: `3px solid ${Color.contextual.danger}` }}
+    >
+      Reset
+    </TimerButton>
+
+  </TimerContent>
 }
 
 export default Timer;
