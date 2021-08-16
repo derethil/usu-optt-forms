7babf48e791ed6540b8d9410a9bced690b15a77a
Jaren Glenn <jarenglenn@gmail.com>
2021-08-16 11:11:30 -0600

    deploy to heroku

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,9 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
   mode: "production",
-  devtool: 'source-map',
+  performance: {
+    hints: false,
+  },
 });
