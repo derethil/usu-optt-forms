2230c92cd42edbd8f1e3276382c3d3656512416f
Jaren Glenn <jarenglenn@gmail.com>
2021-08-16 10:59:34 -0600

    deploy to heroku

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,10 @@
 __version__ = '0.1.0'
 
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 
 from app.config import Config
 from app.helpers import react_uri
 
-db = SQLAlchemy()
-migrate = Migrate()
-
-from app.models import User
 from app.routes.base import base
 
 def create_app():
@@ -18,17 +12,8 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
-    # Extension Setup
-    db.init_app(app)
-    migrate.init_app(app, db)
-
     # Asset Setup
     app.register_blueprint(base)
-
-    # Contexts
-    @app.shell_context_processor
-    def make_shell_context():
-        return {"db": db, "User": User}
 
     @app.context_processor
     def inject_react_url():
