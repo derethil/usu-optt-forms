0d86565a62e61198eef0d404808b96cebfd180de
Jaren Glenn <jarenglenn@gmail.com>
2021-09-15 16:29:51 -0600

    change

--- app/webpack/currentForm.ts
+++ app/webpack/currentForm.ts
@@ -7,4 +7,4 @@ const CURRENT_FORM: formOptions = formOptions.studentTeaching;
 
 export default CURRENT_FORM;
 
-// NOTE: When this is changed, the application data must be cleared in the dev tools or the app will crash
+// NOTE: When this is changed, the application data must be cleared using dev tools or the app will crash
