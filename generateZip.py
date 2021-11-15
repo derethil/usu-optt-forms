03dc8dd932f15b7fa52ac0d9a66820d696fabdf5
Jaren Glenn <jarenglenn@gmail.com>
2021-11-15 15:48:48 -0700

    improve generateZip script

--- generateZip.py
+++ generateZip.py
@@ -2,15 +2,16 @@ import os
 from shutil import copyfile, make_archive
 
 STATIC_PATH = "./app/static"
-
 ZIP_PATH = "./zipStructure"
 
-name = input("What form is this?\n")
 
-copyfile(f"{STATIC_PATH}/js/main.js", f"{ZIP_PATH}/src/js/main.js")
+def main():
+    name = input("What form is this?\n")
+    copyfile(f"{STATIC_PATH}/js/main.js", f"{ZIP_PATH}/src/js/main.js")
+    os.rename(f"{ZIP_PATH}/ObservationForm.html", f"{ZIP_PATH}/{name}.html")
+    make_archive(name, "zip", ZIP_PATH)
+    os.rename(f"{ZIP_PATH}/{name}.html", f"{ZIP_PATH}/ObservationForm.html")
 
-os.rename(f"{ZIP_PATH}/ObservationForm.html", f"{ZIP_PATH}/{name}.html")
 
-make_archive(name, "zip", ZIP_PATH)
-
-os.rename(f"{ZIP_PATH}/{name}.html", f"{ZIP_PATH}/ObservationForm.html")
+if __name__ == "__main__":
+    main()
