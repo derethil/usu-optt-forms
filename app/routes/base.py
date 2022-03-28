f88f13b0344425c317838301ca2a229a427a831d
Jaren Glenn <jarenglenn@gmail.com>
2022-03-28 09:57:33 -0600

    removed useless tiny flask backend

--- app/routes/base.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template, abort
-from jinja2 import TemplateNotFound
-base = Blueprint("base", __name__)
-
-@base.route("/", methods=["GET"])
-def index():
-    return render_template("index.html")
