9e06e7b2525bc8c23b658be31c74910b94b618dc
Jaren Glenn <jarenglenn@gmail.com>
2022-04-22 12:13:32 -0600

    centered 404 content

--- app/webpack/pages/NotFound.tsx
+++ app/webpack/pages/NotFound.tsx
@@ -1,11 +1,17 @@
 import React from "react";
+import styled from "styled-components";
 
 import { PageContent } from "../styledComponents/style";
+
+const CenteredTitle = styled.h1`
+  margin: 1em auto;
+  width: 10em;
+`;
 
 const NotFound = () => {
   return (
     <PageContent>
-      <h1>404 - Not Found</h1>
+      <CenteredTitle>404 - Not Found</CenteredTitle>
     </PageContent>
   );
 };
