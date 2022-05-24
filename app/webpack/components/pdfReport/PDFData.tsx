d7dd2d2f5d22b314a9c5329d5d6f94fb8a439cdb
Jaren Glenn <jarenglenn@gmail.com>
2022-05-24 11:50:27 -0600

    moved feedback generation to its own file

--- app/webpack/components/pdfReport/PDFData.tsx
+++ app/webpack/components/pdfReport/PDFData.tsx
@@ -16,6 +16,7 @@ import generateFormInfoBody from "./formInfoGenerator";
 import { generateRubric } from "./rubric";
 import { useSelectAll } from "../../hooks/hooks";
 import { generateObservations } from "./observations";
+import { generateFeedback } from "./feedback";
 
 // Component that provides PDF generation and the button to do so.
 
@@ -128,35 +129,7 @@ const PDFData = () => {
       });
     }
 
-    // Feedback
-
-    generator.table({
-      head: ["Feedback"],
-    });
-
-    const feedbackRows = [
-      [feedbackLabel(currentForm, 1), data.feedback.area1],
-      [feedbackLabel(currentForm, 2), data.feedback.area2],
-      [feedbackLabel(currentForm, 3), data.feedback.area3],
-    ];
-
-    if (currentForm === formOptions.selfEvaluation) {
-      feedbackRows.pop();
-      feedbackRows.push(
-        [feedbackLabel(currentForm, 4), data.feedback.area4],
-        [feedbackLabel(currentForm, 5), data.feedback.area5]
-      );
-    }
-
-    feedbackRows.forEach(([title, comment], index) => {
-      const startY = (generator.pdf as any).lastAutoTable.finalY + 2;
-      generator.table({
-        startY: index === 0 ? startY : "RELATIVE",
-        head: [title as string],
-        headStyles: { fillColor: Color.blues.blue },
-        body: [[comment]],
-      });
-    });
+    generateFeedback(generator, data);
 
     // Save
     const date = new Date(data.formInfo.date);
---
+++ app/webpack/components/pdfReport/feedback.ts
@@ -0,0 +1,35 @@
+import currentForm, { formOptions } from "../../currentForm";
+import Color from "../../styledComponents/colors";
+import { IData } from "../../types/types";
+import { feedbackLabel } from "../../utils/utils";
+import PDFGenerator from "./PDFGenerator";
+
+export function generateFeedback(generator: PDFGenerator, data: IData) {
+  generator.table({
+    head: ["Feedback"],
+  });
+
+  const feedbackRows = [
+    [feedbackLabel(currentForm, 1), data.feedback.area1],
+    [feedbackLabel(currentForm, 2), data.feedback.area2],
+    [feedbackLabel(currentForm, 3), data.feedback.area3],
+  ];
+
+  if (currentForm === formOptions.selfEvaluation) {
+    feedbackRows.pop();
+    feedbackRows.push(
+      [feedbackLabel(currentForm, 4), data.feedback.area4],
+      [feedbackLabel(currentForm, 5), data.feedback.area5]
+    );
+  }
+
+  feedbackRows.forEach(([title, comment], index) => {
+    const startY = (generator.pdf as any).lastAutoTable.finalY + 2;
+    generator.table({
+      startY: index === 0 ? startY : "RELATIVE",
+      head: [title as string],
+      headStyles: { fillColor: Color.blues.blue },
+      body: [[comment]],
+    });
+  });
+}
